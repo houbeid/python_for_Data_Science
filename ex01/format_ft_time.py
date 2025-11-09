01fd736351336914450265744cede0eed21717ed
Oubeid Hamad <OubeidH@algoconsulting.net>
2025-11-09 23:31:19 +0000

    edit ex02

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -1,5 +1,7 @@
 import time
 from datetime import datetime
 
-print ("Seconds Since January 1, 1970: ", time.time() )
-print (datetime.now().date())
+seconds = time.time()
+print(f"Seconds since January 1, 1970: {seconds:.4e}")
+current_date = datetime.now().strftime("%b %d %Y")
+print(current_date)
